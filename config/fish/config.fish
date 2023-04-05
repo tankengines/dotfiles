23eb1ba53d0685fbdb92783e828ed1926d04b89c
Thomas <hi@itsthomas.tech>
2023-04-06 00:27:35 +0800

    Changes

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -2,6 +2,11 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+abbr --add  .. cd ..
+abbr --add ... cd ../..
+abbr --add .... cd ../../..
+abbr --add ..... cd ../../../..
+
 set fish_greeting
 starship init fish | source
 

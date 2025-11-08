b1fa6204b16ae02e693d57ecf122a92a04cf1035
Thomas <hi@thomasng.me>
2025-11-08 23:03:32 +0000

    Add starship

--- sync.sh
+++ sync.sh
@@ -4,5 +4,6 @@ ln -sfn ~/dotfiles/helix ~/.config/helix
 ln -sfn ~/dotfiles/vim ~/.vim
 ln -sfn ~/dotfiles/zellij ~/.config/zellij
 
+ln -sf ~/dotfiles/starship.toml ~/.config/starship.toml 
 ln -sf ~/dotfiles/zshrc ~/.zshrc
 

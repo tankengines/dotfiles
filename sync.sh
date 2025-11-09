80e3fe6319206ab605e15f1775b6e79b7f864430
Thomas <hi@thomasng.me>
2025-11-09 23:41:15 +0000

    alacritty

--- sync.sh
+++ sync.sh
@@ -3,6 +3,7 @@
 ln -sfn ~/dotfiles/helix ~/.config/helix
 ln -sfn ~/dotfiles/vim ~/.vim
 ln -sfn ~/dotfiles/zellij ~/.config/zellij
+ln -sfn ~/dotfiles/alacritty ~/.config/alacritty
 
 ln -sf ~/dotfiles/starship.toml ~/.config/starship.toml 
 ln -sf ~/dotfiles/zshrc ~/.zshrc

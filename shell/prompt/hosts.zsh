17558401f09676c5b4e8c6dcc77871270a472949
Thomas <hi@thomasng.me>
2026-01-28 22:08:58 +0000

    single file zshrc

--- shell/prompt/hosts.zsh
+++
@@ -1,9 +0,0 @@
-typeset -A HOST_ALIAS
-HOST_ALIAS=(
-  Thomas-PowerBook-G3.local mbp 
-)
-
-get_host() {
-  local h=${HOST:-$(hostname)}
-  print -n -- ${HOST_ALIAS[$h]:-$h}
-}
--- zshrc.zsh
+++ zshrc.zsh
@@ -1,4 +1,4 @@
-if [[ -e "$HOME/dotfiles/shell/lag/zsh-nvm.zsh" ]]; then
+if [[ -e "$HOME/dotfiles/shell/lang/zsh-nvm.zsh" ]]; then
   source "$HOME/dotfiles/shell/lang/zsh-nvm.zsh"
 fi
 
@@ -31,25 +31,112 @@ if [[ -d $COMMANDS ]]; then
   done
 fi
 
-PROMPT_MODULES="${HOME}/dotfiles/shell/prompt"
-if [[ -d $PROMPT_MODULES ]]; then
+LANGS="${HOME}/dotfiles/shell/lang"
+if [[ -d $LANGS ]]; then
   setopt localoptions null_glob
-  for f in "${PROMPT_MODULES}"/*.zsh; do
+  for f in "${LANGS}"/*.zsh; do
     if [[ -f $f && -r $f ]]; then
       source "$f"
     fi
   done
 fi
 
-precmd_functions=()
-# defensive checking; only add to precmd if these are found
-if typeset -f build_exit_prefix >/dev/null; then
-  precmd_functions+=(build_exit_prefix)
-fi
+################
+# PROMPT STUFF #
+################
+typeset -A HOST_ALIAS
+HOST_ALIAS=(
+  Thomas-PowerBook-G3.local mbp 
+  ThomasPerBookG3 mbp
+)
 
-if typeset -f time_command >/dev/null; then
-  precmd_functions+=(time_command)
-fi
+get_host() {
+  local h=${HOST:-$(hostname)}
+  print -n -- ${HOST_ALIAS[$h]:-$h}
+}
+
+build_exit_prefix() {
+  local code=$?
+  if (( code == 130 || code == 2 )); then
+    EXIT_PREFIX="%F{white}[%F{yellow}${code}%F{white}]%f "
+  elif (( code != 0 )); then
+    EXIT_PREFIX="%F{white}[%F{red}${code}%F{white}]%f "
+  else
+    EXIT_PREFIX=""
+  fi
+}
+
+get_git_info() {
+  # are we in a git repo?
+  git rev-parse --is-inside-work-tree &>/dev/null || return
+
+  local branch=$(git symbolic-ref --short HEAD 2>/dev/null || git rev-parse --short HEAD 2>/dev/null)
+  local git_status=""
+  local color="green"
+
+  # dirty
+  if ! git diff-index --quiet HEAD -- 2>/dev/null; then
+    git_status="*"
+    color="yellow"
+  fi
+
+  # untracked
+  if [[ -n $(git ls-files --others --exclude-standard 2>/dev/null) ]]; then
+    color="red"
+  fi
+
+  if [[ -n $branch ]]; then
+    print -n -- "%F{white}:%F{${color}}${branch}${git_status}%f"
+  fi
+}
+
+# https://derrick.blog/2022/12/21/command-timing-in-zsh/
+# but I used date instead of gdate
+preexec() {
+    timer=$(($(date +%s%N)/1000000))
+}
+
+converts() {
+    local t=$1
+
+    local d=$((t / 1000 / 60 / 60 / 24))
+    local h=$((t / 1000 / 60 / 60 % 24))
+    local m=$((t / 1000 / 60 % 60))
+    local s=$((t / 1000 % 60))
+    local ms=$((t % 1000))
+
+    if [[ $s -lt 1 ]]; then
+        return
+    fi
+
+    local result=""
+    [[ $d -gt 0 ]] && result+=" ${d}d"
+    [[ $h -gt 0 ]] && result+=" ${h}h"
+    [[ $m -gt 0 ]] && result+=" ${m}m"
+    [[ $s -gt 0 ]] && result+=" ${s}s"
+    [[ $ms -gt 0 ]] && result+=" ${ms}ms"
+    
+    echo "$result"
+}
+
+time_command() {
+    if [[ -n "$timer" ]]; then
+        now=$(($(date +%s%N)/1000000))
+        elapsed=$((now - timer))
+
+        reset_color='%f'
+        RPROMPT="%F{cyan} $(converts "$elapsed") $reset_color"
+        export RPROMPT
+        unset timer
+    else
+        # reset if no command
+        RPROMPT=""
+    fi
+}
+
+precmd_functions=()
+precmd_functions+=(build_exit_prefix)
+precmd_functions+=(time_command)
 
 autoload -Uz colors && colors
 setopt prompt_subst
